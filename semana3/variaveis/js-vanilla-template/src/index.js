b5cc9174b5e1f618bb3e4dd975dbe60a9d369621
frank159 <frankcf60@gmail.com>
2021-02-22 16:41:12 -0300

    resolvi-os-exercicios

---
+++ semana3/variaveis/js-vanilla-template/src/index.js
@@ -0,0 +1,75 @@
+/*
+1.  10
+    10 5
+
+2.  10 20 a
+*/ 
+
+//---1--// 
+
+// let nome 
+
+// let idade
+
+// console.log('primeira variavel é do tipo', typeof nome, 'e a segunda é ', typeof idade)
+
+// //não foi impresso nenhum tipo no console pelo valor ser nulo 
+
+// nome = prompt('Qual seu nome ?')
+// idade = prompt('E qual tua idade ?')
+
+// console.log('agora o primeira variavel é do tipo', typeof nome, 'e a segunda é', typeof idade)
+
+// console.log( 'Olá', nome, ',você tem', idade, 'anos') 
+
+//--2--//
+
+// let filme = prompt ( 'Olá, qual seu filme fávorito?' )
+// let livro = prompt ( 'Qual seu livro fávorito' )
+// let pc = prompt ( 'Usa pc ou notebook?' )
+// let mouse = prompt ( 'Qual sua marca de mouse?' )
+// let teclado = prompt ( 'E de teclado?' )
+
+// console.log( '1.Olá, qual seu filme fávorito?'   )
+// console.log('RESPOSTA: ', filme)
+
+// console.log('Qual seu livro fávorito')
+// console.log('RESPOSTA: ', livro)
+
+// console.log('Usa pc ou notebook?')
+// console.log('RESPOSTA: ', pc)
+
+// console.log( 'Qual sua marca de mouse?' )
+// console.log('RESPOSTA: ', mouse)
+
+// console.log( 'E de teclado?' )
+// console.log('RESPOSTA: ', teclado)
+
+//--3--//
+
+// let comidas = ["churros", "pizza", "esfira", "cogumelos", "queijo"]
+
+// console.log(comidas)
+
+// console.log('Essas são as minhas comidas preferidas:')
+// console.log(comidas[0])
+// console.log(comidas[1])
+// console.log(comidas[2])
+// console.log(comidas[3])
+// console.log(comidas[4])
+
+//--4--//
+
+camisa = prompt ( 'sua camisa é verde?' )
+calça = prompt ( 'sua calça é azul?' )
+meia = prompt ( 'sua meia é preta?' ) 
+
+const perguntas = [ camisa, calça, meia]
+perguntas[0] = item 1
+perguntas[1] = 
+
+console.log('sua camisa é verde?',repostas)
+console.log('sua calça é azul?',repostas)
+console.log('sua meia é preta?'repostas)
+
+//NÃO CONSEGUI RESOLVER
