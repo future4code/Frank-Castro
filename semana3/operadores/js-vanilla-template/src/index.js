379dd83199ca927f9e932a8b5758a078142321fe
frank159 <frankcf60@gmail.com>
2021-02-23 15:24:39 -0300

    resolvi-os-exercicios

---
+++ semana3/operadores/js-vanilla-template/src/index.js
@@ -0,0 +1,84 @@
+//--1--//
+
+// a.  false
+// b.  false
+// c.  true
+// e.  boolean
+
+//--2--//
+
+//a. Undefined
+//b. null
+//c. 11
+//d. 3
+//e.(11) [3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+// 0: 3
+// 1: 19
+// 2: 5
+// 3: 6
+// 4: 7
+// 5: 8
+// 6: 9
+// 7: 10
+// 8: 11
+// 9: 12
+// 10: 13
+//f. 9
+
+//-------------------------------------//
+
+//--1--//
+
+// const idade = prompt ("qual sua idade?")
+// console.log(idade)
+
+// const idadem = prompt ("qual a idade do seu melhor amigo?")
+// console.log(idadem)
+
+// window.alert ("Sua idade é maior do que a do seu melhor amigo?")
+// console.log("Sua idade é maior do que a do seu melhor amigo?")
+
+// const maior = Number(idade) > Number(idadem) 
+// console.log("você é mais velho que seu amigo?", maior)
+
+// const diferença = Number(idade) - Number(idadem)
+// console.log("a diferença de idade é: ", diferença)
+
+//--2--//
+
+// const par = prompt ("insira um número par") 
+// Number(par)
+// console.log(par)
+
+// const divisao = par/2  
+// console.log(divisao)
+
+//o padrão é que sempre é metade pois estou dividindo por 2
+//quando divido um número impar ele coloca o resultado com virgula 
+
+//--3--//
+
+// let listaDeTarefas = []
+
+// const PlistaDeTarefas = prompt("Qual sua primeira tarefa?")
+// const SlistaDeTarefas = prompt("Qual sua segunda tarefa?")
+// const TlistaDeTarefas = prompt("Qual sua terceira tarefa?")
+// listaDeTarefas.push(PlistaDeTarefas)
+// listaDeTarefas.push(SlistaDeTarefas)
+// listaDeTarefas.push(TlistaDeTarefas)
+// console.log(listaDeTarefas)
+
+// const indice = prompt("remova uma tarefa")
+// listaDeTarefas.splice(indice)
+
+// console.log(listaDeTarefas) 
+
+//--4--//
+
+// const nome = prompt("qual seu nome?")
+// console.log(nome)
+
+// const email = prompt("qual seu email?")
+// console.log(email)
+
+// window.alert(`O e-mail ${email} foi cadastrado com sucesso. Seja bem-vinda(o), ${nome}`)
