a316dd9efe18846d6d6f686c00af7aaf88b06b4e
frank159 <frankcf60@gmail.com>
2021-05-25 20:13:08 -0300

    terminando exercicios

--- semana15/aula45/exer1-1.sql
+++ semana15/aula45/exer1-1.sql
@@ -1,9 +1,31 @@
+SHOW DATABASES;
+
 CREATE TABLE Actor (
     id VARCHAR(255) PRIMARY KEY,
     name VARCHAR (255) NOT NULL,
     salary FLOAT NOT NULL,
     birth_date DATE NOT NULL,
     gender VARCHAR(6) NOT NULL
+);
+
+CREATE TABLE Moves (
+    id VARCHAR(255) PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    synopsis TEXT NOT NULL,
+    release_date DATE NOT NULL,
+    assessment FLOAT NOT NULL
+);
+
+INSERT INTO Moves (id, title, synopsis, release_date, assessment)
+VALUES(
+  "004", 
+  "Bacurau",
+  "Os moradores de Bacurau, um pequeno povoado do sertão brasileiro, descobrem que a comunidade não consta mais em qualquer mapa. 
+  Aos poucos, eles percebem algo estranho na região: enquanto drones passeiam pelos céus, estrangeiros chegam à cidade. Quando carros são 
+  baleados e cadáveres começam a aparecer, Teresa, Domingas, Acácio, Plínio, Lunga e outros habitantes chegam à conclusão de que estão sendo atacados. 
+  Agora, o grupo precisa identificar o inimigo e criar coletivamente um meio de defesa.",
+  "2019-08-23", 
+  8
 );
 
 INSERT INTO Actor (id, name, salary, birth_date, gender)
@@ -48,5 +70,14 @@ VALUES(
   "Juliana Paes",
   719333.33,
   "1979-03-26", 
+  "female"
+);
+
+INSERT INTO Actor (id, name, salary, birth_date, gender)
+VALUES(
+  "006", 
+  "Jigiane de gi",
+  800000,
+  "1979-03-27", 
   "female"
 );
--- semana15/aula45/exer1.sql
+++ semana15/aula45/exer1.sql
@@ -1,4 +1,9 @@
-SELECT * FROM Actor;
+SELECT * FROM Actor
+WHERE 
+(name LIKE "%g%" OR name LIKE "%G%" OR name LIKE "%a%" OR name LIKE "%A%")
+AND salary BETWEEN 350000 AND 900000;
+
+SELECT * FROM Moves;
 
 SELECT id, salary from Actor;
 
@@ -8,4 +13,22 @@ SELECT id, name from Actor WHERE gender = "female";
 
 SELECT salary from Actor WHERE name = "Tony Ramos";
 
-SELECT * FROM Actor;
+SELECT * FROM Actor WHERE gender = "invalid";
+/* DEU NULL */
+
+SELECT id, name, salary from Actor WHERE salary < 500000;
+
+SELECT id, nome from Actor WHERE id = "002";
+/* NAME ESTAVA ESCRITO EM PORTUGUES  */
+
+SELECT id, name from Actor WHERE id = "002";
+
+SELECT id, title from Moves WHERE id = "002";
+
+SELECT * FROM Moves
+WHERE title LIKE "%B%" OR synopsis LIKE "%b%";
+
+SELECT * FROM Moves
+WHERE release_date < "2020-05-04";
+
+
